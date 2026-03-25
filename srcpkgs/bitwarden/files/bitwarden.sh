b126550eae2ceca37ccda0be2d5aa5c7983c7ebc
creations <creations@creations.works>
2026-03-25 08:41:45 -0400

    [bitwarden] fix tray on wayland

--- srcpkgs/bitwarden/files/bitwarden.sh
+++ srcpkgs/bitwarden/files/bitwarden.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 ulimit -c 0
 
-PARAMS="--ozone-platform=x11"
-if [ "$USE_X11" = "false" ]; then
+if [ "$XDG_SESSION_TYPE" = "wayland" ] && [ "$USE_X11" != "true" ]; then
 	PARAMS="--enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto"
+else
+	PARAMS="--ozone-platform=x11"
 fi
 
 exec /usr/lib/bitwarden/bitwarden-app $PARAMS "$@"
